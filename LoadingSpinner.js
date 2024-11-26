03cccb5d42e61086ff7f04bf8b68424661762608
Jiri Benes <benes.jiri@shoppingarena.eu>
2024-11-26 19:10:00 +0200

    refactor

---
+++ LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LoadingSpinner() {
+    return (
+        <div className="spinner">
+            <div className="double-bounce1"></div>
+            <div className="double-bounce2"></div>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
+
+
